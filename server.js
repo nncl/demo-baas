5f5398be6947c96ef94ce145afd942aa940731ec
logonrm <logonrm@fiap.com.br>
2017-09-04 20:10:56 -0300

    Add profile

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const express = require("express")
+      , app = express()
+      , port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log("Server running on", port);
+});
